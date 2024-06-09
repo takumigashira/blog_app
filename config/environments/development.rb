942dfaaa5bf05b4d5442b1bd337e573acbba2f38
takumi <takumigashira@gmail.com>
2024-06-09 12:34:43 +0900

    access allow host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.hosts << "ik1-117-65334.vs.sakura.ne.jp"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
